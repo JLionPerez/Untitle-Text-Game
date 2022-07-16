8ff7ebf3f803167704bcd7ad168f94f5bc68db87
joellePerez <joelle.perez.1222@mail.linnbenton.edu>
2022-07-16 00:59:38 -0700

    added functions for heal and mana

--- player.py
+++ player.py
@@ -1,3 +1,6 @@
+from audioop import add
+
+
 class Player:
     def __init__(self, name, health, mana, style):
         self.name = name
@@ -5,4 +8,17 @@ class Player:
         self.mana = 15
         self.style = style
 
+    #updates health when player receives damage or restores health
+    def add_heal(self, added_pts):
+        if added_pts < 0:
+            self.health = self.health - added_pts
+        else:
+            self.health = self.health + added_pts
     
+    #updates mana when player spend mana or restores mana
+    def add_mana(self, added_pts):
+        if added_pts < 0:
+            self.mana = self.mana - added_pts
+        else:
+            self.mana = self.mana + added_pts
+
