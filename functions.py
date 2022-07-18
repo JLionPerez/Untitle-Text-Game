2e1d2cd26279b7fdd256b05e1ff471512e55b448
Joelle Perez <j.perez.code@gmail.com>
2022-07-17 22:42:48 -0700

    add functions for what the player can do
    Signed-off-by: Joelle Perez <j.perez.code@gmail.com>

---
+++ functions.py
@@ -0,0 +1,32 @@
+import player
+
+#attack spells
+attack_spells = [
+    {'name':'Ether', 'points':5, 'cost':5},
+    {'name':'Flames', 'points':10, 'cost':10}
+]
+
+#heal spells
+heal_spells = [
+    {'name':'Herb', 'points':5, 'cost':5},
+    {'name':'Blessed Potion', 'points': 10, 'cost':10}
+]
+
+#mana heal spells
+restore_mana_spells = [
+    {'name':'Ether', 'points':10, 'cost':5},
+    {'name':'Elixir', 'points':15, 'cost':10}
+]
+
+#what can the player do?
+
+#cast attack spells
+
+
+#cast heal spells
+
+
+#add health
+#remove health
+#add spells
+#remove spells
